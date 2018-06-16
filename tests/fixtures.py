9b4bd6b32f3fd9e7a6734ed1fce5b361a91d4070
insightindustry <software@insightindustry.com>
2018-06-16 18:49:40 -0400

    Implemented passing unit tests for primary key retrieval. Closes #1

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -14,7 +14,7 @@ import pytest
 from sqlathanor import BaseModel as Base
 
 from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, MetaData
-from sqlalchemy.orm import relationship, clear_mappers
+from sqlalchemy.orm import relationship, clear_mappers, Session
 from sqlalchemy.ext.declarative import declarative_base
 
 class State(object):
@@ -33,29 +33,17 @@ def state(request):
         scope = "session"
     )
 
-@pytest.yield_fixture
-def base_model(request):
-
+@pytest.fixture(scope = "session")
+def db_engine(request):
     engine = create_engine('sqlite://')
 
+    return engine
+
+@pytest.yield_fixture(scope = 'session')
+def tables(request, db_engine):
     BaseModel = declarative_base(cls = Base, metadata = MetaData())
-    BaseModel.metadata.clear()
-    BaseModel.metadata.create_all(engine)
 
-    yield BaseModel
-
-    clear_mappers()
-    BaseModel.metadata.drop_all(engine)
-    BaseModel.metadata.clear()
-
-
-@pytest.fixture
-def model_single_pk(request, base_model):
-
-    User = None
-    Address = None
-
-    class User(base_model):
+    class User(BaseModel):
         """Mocked class with a single primary key."""
 
         __tablename__ = 'users'
@@ -67,7 +55,7 @@ def model_single_pk(request, base_model):
                       String(50))
         addresses = relationship('Address', backref = 'user')
 
-    class Address(base_model):
+    class Address(BaseModel):
         """Mocked class with a single primary key."""
 
         __tablename__ = 'addresses'
@@ -81,16 +69,10 @@ def model_single_pk(request, base_model):
                          Integer,
                          ForeignKey('users.id'))
 
-    return (User, Address)
-
-
-@pytest.fixture
-def model_composite_pk(request, base_model):
-
-    class User2(base_model):
+    class User2(BaseModel):
         """Mocked class with a single primary key."""
 
-        __tablename__ = 'users'
+        __tablename__ = 'users_composite'
 
         id = Column('id',
                     Integer,
@@ -105,10 +87,10 @@ def model_composite_pk(request, base_model):
                       String(50))
         addresses = relationship('Address2', backref = 'user')
 
-    class Address2(base_model):
+    class Address2(BaseModel):
         """Mocked class with a single primary key."""
 
-        __tablename__ = 'addresses'
+        __tablename__ = 'addresses_composite'
 
         id = Column('id',
                     Integer,
@@ -117,6 +99,78 @@ def model_composite_pk(request, base_model):
                        String(50))
         user_id = Column('user_id',
                          Integer,
-                         ForeignKey('users.id'))
+                         ForeignKey('users_composite.id'))
+
+    BaseModel.metadata.create_all(db_engine)
+
+    yield {
+        'base_model': BaseModel,
+        'model_single_pk': (User, Address),
+        'model_composite_pk': (User2, Address2)
+    }
+
+    clear_mappers()
+    BaseModel.metadata.drop_all(db_engine)
+    BaseModel.metadata.clear()
+
+@pytest.yield_fixture(scope = 'session')
+def base_model(request, tables):
+    BaseModel = tables['base_model']
+
+    yield BaseModel
+
+
+@pytest.yield_fixture
+def db_session(request, db_engine, base_model):
+    connection = db_engine.connect()
+    transaction = connection.begin()
+    session = Session(bind = connection)
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+@pytest.fixture()
+def model_single_pk(request, tables):
+    User = tables['model_single_pk'][0]
+    Address = tables['model_single_pk'][1]
+
+    return (User, Address)
+
+
+@pytest.fixture()
+def instance_single_pk(request, model_single_pk):
+    instance_values = {
+        'id': 1,
+        'name': 'Test Name'
+    }
+    user = model_single_pk[0]
+
+    instance = user(**instance_values)
+
+    return (instance, instance_values)
+
+
+@pytest.fixture(scope = 'session')
+def model_composite_pk(request, tables):
+    User2 = tables['model_composite_pk'][0]
+    Address2 = tables['model_composite_pk'][1]
 
     return (User2, Address2)
+
+
+@pytest.fixture
+def instance_composite_pk(request, model_composite_pk):
+    instance_values = {
+        'id': 1,
+        'id2': 2,
+        'id3': 3,
+        'name': 'Test Name'
+    }
+    user = model_composite_pk[0]
+
+    instance = user(**instance_values)
+
+    return (instance, instance_values)
--- tests/test_primary_key_retrieval.py
+++ tests/test_primary_key_retrieval.py
@@ -13,32 +13,23 @@ import pytest
 
 from sqlalchemy import Column
 
-from tests._fixtures import base_model, model_single_pk, model_composite_pk
+from tests.fixtures import db_engine, tables, base_model, db_session, \
+    model_single_pk, model_composite_pk, instance_single_pk, instance_composite_pk
 
 
-@pytest.mark.parametrize('models, instantiate, expected_count, instance_values', [
-    (model_single_pk, False, 1, None),
-    (model_composite_pk, False, 3, None),
-    (model_single_pk, True, 1, {
-        'id': 1,
-        'name': 'test name'
-    }),
-    (model_composite_pk, True, 3, {
-        'id': 1,
-        'id2': 123,
-        'id3': 456,
-        'name': 'test name',
-    }),
+@pytest.mark.parametrize('expected_count', [
+    (1),
+    (3),
 ])
 @pytest.mark.filterwarnings('ignore:This declarative base already contains a class')
-def test_get_primary_key_columns(request, base_model, models, instantiate, expected_count, instance_values):
-    models = models(request, base_model)
-
-    if not instantiate:
-        target = models[0]
-    else:
-        model = models[0]
-        target = model(**instance_values)
+def test_get_primary_key_columns_classmethod(request,
+                                             model_single_pk,
+                                             model_composite_pk,
+                                             expected_count):
+    if expected_count == 1:
+        target = model_single_pk[0]
+    elif expected_count > 1:
+        target = model_composite_pk[0]
 
     primary_key_columns = target.get_primary_key_columns()
 
@@ -48,29 +39,46 @@ def test_get_primary_key_columns(request, base_model, models, instantiate, expec
         assert isinstance(column, Column)
 
 
-@pytest.mark.parametrize('models, instantiate, expected_count, instance_values, expected_names', [
-    (model_single_pk, False, 1, None, ['id']),
-    (model_composite_pk, False, 3, None, ['id', 'id2', 'id3']),
-    (model_single_pk, True, 1, {
-        'id': 1,
-        'name': 'test name'
-    }, ['id']),
-    (model_composite_pk, True, 3, {
-        'id': 1,
-        'id2': 123,
-        'id3': 456,
-        'name': 'test name',
-    }, ['id', 'id2', 'id3']),
+@pytest.mark.parametrize('expected_count', [
+    (1),
+    (3),
 ])
 @pytest.mark.filterwarnings('ignore:This declarative base already contains a class')
-def test_get_primary_key_column_names(request, base_model, models, instantiate, expected_count, instance_values, expected_names):
-    models = models(request, base_model)
+def test_get_primary_key_columns_instance(request,
+                                          instance_single_pk,
+                                          instance_composite_pk,
+                                          expected_count):
+    if expected_count == 1:
+        instances = instance_single_pk
+    elif expected_count > 1:
+        instances = instance_composite_pk
 
-    if not instantiate:
-        target = models[0]
-    else:
-        model = models[0]
-        target = model(**instance_values)
+    target = instances[0]
+    instance_values = instances[1]
+
+    primary_key_columns = target.get_primary_key_columns()
+
+    assert len(primary_key_columns) == expected_count
+
+    for column in primary_key_columns:
+        assert isinstance(column, Column)
+
+
+@pytest.mark.parametrize('expected_count, expected_names', [
+    (1, ['id']),
+    (3, ['id', 'id2', 'id3']),
+])
+@pytest.mark.filterwarnings('ignore:This declarative base already contains a class')
+def test_get_primary_key_column_names_classmethod(request,
+                                                  model_single_pk,
+                                                  model_composite_pk,
+                                                  expected_count,
+                                                  expected_names):
+
+    if expected_count == 1:
+        target = model_single_pk[0]
+    elif expected_count > 1:
+        target = model_composite_pk[0]
 
     pk_column_names = target.get_primary_key_column_names()
 
@@ -79,3 +87,72 @@ def test_get_primary_key_column_names(request, base_model, models, instantiate, 
     for column in pk_column_names:
         assert isinstance(column, str)
         assert column in expected_names
+
+
+@pytest.mark.parametrize('expected_count, expected_names', [
+    (1, ['id']),
+    (3, ['id', 'id2', 'id3']),
+])
+@pytest.mark.filterwarnings('ignore:This declarative base already contains a class')
+def test_get_primary_key_column_names_instance(request,
+                                               instance_single_pk,
+                                               instance_composite_pk,
+                                               expected_count,
+                                               expected_names):
+    if expected_count == 1:
+        target = instance_single_pk[0]
+        instance_values = instance_single_pk[1]
+    elif expected_count > 1:
+        target = instance_composite_pk[0]
+        instance_values = instance_composite_pk[1]
+
+    pk_column_names = target.get_primary_key_column_names()
+
+    assert len(pk_column_names) == expected_count
+
+    for column in pk_column_names:
+        assert isinstance(column, str)
+        assert column in expected_names
+
+
+
+@pytest.mark.parametrize('is_composite, expected_count', [
+    (False, None),
+    (True, None),
+    (False, 1),
+    (True, 3),
+])
+@pytest.mark.filterwarnings('ignore:This declarative base already contains a class')
+def test_primary_key_value(request,
+                           db_session,
+                           instance_single_pk,
+                           instance_composite_pk,
+                           is_composite,
+                           expected_count):
+    if is_composite or (expected_count is not None and expected_count > 1):
+        target = instance_composite_pk[0]
+        instance_values = instance_composite_pk[1]
+    else:
+        target = instance_single_pk[0]
+        instance_values = instance_single_pk[1]
+
+    if expected_count:
+        db_session.add(target)
+        db_session.commit()
+
+    pk_values = target.primary_key_value
+
+    if expected_count is None:
+        assert pk_values is None
+    elif expected_count == 1:
+        assert not isinstance(pk_values, tuple)
+        assert pk_values == instance_values['id']
+    elif expected_count > 1:
+        assert isinstance(pk_values, tuple)
+        assert len(pk_values) == expected_count
+
+        id_values = [instance_values[key] for key in instance_values
+                     if 'id' in key]
+
+        for index, value in enumerate(pk_values):
+            assert pk_values[index] == id_values[index]
