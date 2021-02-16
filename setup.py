57decb730884177368f9f573c1e85639535150ac
Chris Modzelewski <software@insightindustry.com>
2021-02-16 18:22:39 -0500

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -181,7 +181,7 @@ setup(
                  'tox',
                  'codecov',
                  'Flask-SQLAlchemy',
-                 'pydantic;pytnon_version>="3.6"'],
+                 'pydantic;python_version>="3.6"'],
     },
 
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
